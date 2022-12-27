f7ab9b65d0472665f636081821b55841a0dfd68d
Aron <aronaland4@gmail.com>
2022-12-27 13:25:26 +0200

    cors uuendus

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ const routes = require("./routes/routes");
 
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: [
+			"http://localhost:5173",
+			"https://cosmos-odyssey-react.vercel.app/",
+		],
 	})
 );
 
